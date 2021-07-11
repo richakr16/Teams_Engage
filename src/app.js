cfa8baee2381c6573340ff1e52ad6cb5c3582553
Richa Kumari <richasimran16@gmail.com>
2021-07-11 08:37:58 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+let express = require( 'express' );
+let app = express();
+let server = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( server );
+let stream = require( './ws/stream' );
+let path = require( 'path' );
+let favicon = require( 'serve-favicon' );
+
+app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/index.html' );
+} );
+
+
+io.of( '/stream' ).on( 'connection', stream );
+
+
+let port = process.env.PORT;
+if(port== null || port == ""){
+  port = 3000;
+}
+server.listen( port, function(){
+  console.log("Server has started successfully");
+} );
